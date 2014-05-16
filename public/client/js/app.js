bc6dfe62533b9a5acf6ca18e4caf853d2bd7d27f
Hack Reactor Students <taylor.harwin@gmail.com>
2014-05-16 16:26:16 -0700

    renders Angular Create Link View

--- public/client/js/app.js
+++ public/client/js/app.js
@@ -1,11 +1,10 @@
 var app = angular.module('myApp', []);
 
-app.controller('linksController', function($scope){
+app.controller('linksController', function($scope, $http){
   console.log($scope);
 
   $http({method: 'GET', url: '/links'})
   .success(function(data, status, headers, config){
-
     $scope.links = data;
   });
 
